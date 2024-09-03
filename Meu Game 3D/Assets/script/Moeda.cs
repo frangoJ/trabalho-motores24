f9f251e26fb2de97649f6cab0dbfb611bc24f6e0
frangoJ <joaolucas142ajuda@gmail.com>
2024-09-03 10:13:14 -0300

    atv dia 03/09/2024
    aula 6 minuto 9

---
+++ Meu Game 3D/Assets/script/Moeda.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidadeGiro * Time.deltaTime, Space.World);
+    }
+}
